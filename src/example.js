afd7577aff522d1c6bd610307e13780539802fe2
Kaleem Elahi <shaikh9996@gmail.com>
2020-01-07 11:49:43 +0530

    Initial commit

---
+++ src/example.js
@@ -0,0 +1,129 @@
+import React, { Component } from "react";
+import chroma from "chroma-js";
+import CreatableSelect from "react-select/creatable";
+
+const components = {
+  DropdownIndicator: null
+};
+
+const colourOptions = [
+  { value: "ocean", label: "Ocean", color: "#00B8D9", isFixed: true },
+  { value: "blue", label: "Blue", color: "#0052CC", isDisabled: true },
+  { value: "purple", label: "Purple", color: "#5243AA" },
+  { value: "red", label: "Red", color: "#FF5630", isFixed: true },
+  { value: "orange", label: "Orange", color: "#FF8B00" },
+  { value: "yellow", label: "Yellow", color: "#FFC400" },
+  { value: "green", label: "Green", color: "#36B37E" },
+  { value: "forest", label: "Forest", color: "#00875A" },
+  { value: "slate", label: "Slate", color: "#253858" },
+  { value: "silver", label: "Silver", color: "#666666" }
+];
+
+const colourStyles = {
+  control: styles => ({ ...styles, backgroundColor: "white" }),
+  option: (styles, { data, isDisabled, isFocused, isSelected }) => {
+    const color = chroma(data.color);
+    return {
+      ...styles,
+      backgroundColor: isDisabled
+        ? null
+        : isSelected
+        ? data.color
+        : isFocused
+        ? color.alpha(0.1).css()
+        : null,
+      color: isDisabled
+        ? "#ccc"
+        : isSelected
+        ? chroma.contrast(color, "white") > 2
+          ? "white"
+          : "black"
+        : data.color,
+      cursor: isDisabled ? "not-allowed" : "default",
+
+      ":active": {
+        ...styles[":active"],
+        backgroundColor:
+          !isDisabled && (isSelected ? data.color : color.alpha(0.3).css())
+      }
+    };
+  },
+  multiValue: (styles, { data }) => {
+    const color = chroma(data.color);
+    return {
+      ...styles,
+      backgroundColor: color.alpha(0.1).css()
+    };
+  },
+  multiValueLabel: (styles, { data }) => ({
+    ...styles,
+    color: data.color
+  }),
+  multiValueRemove: (styles, { data }) => ({
+    ...styles,
+    color: data.color,
+    ":hover": {
+      backgroundColor: data.color,
+      color: "white"
+    }
+  })
+};
+
+const createOption = (label: string) => ({
+  label,
+  value: label,
+  color: "black"
+});
+
+export default class CreatableInputOnly extends Component<*, State> {
+  state = {
+    inputValue: "",
+    value: colourOptions
+  };
+  handleChange = (value: any, actionMeta: any) => {
+    console.group("Value Changed");
+    console.log(value);
+    console.log(`action: ${actionMeta.action}`);
+    console.groupEnd();
+    this.setState({ value });
+  };
+  handleInputChange = (inputValue: string) => {
+    this.setState({ inputValue });
+  };
+  handleKeyDown = (event: SyntheticKeyboardEvent<HTMLElement>) => {
+    const { inputValue, value } = this.state;
+    if (!inputValue) return;
+    switch (event.key) {
+      case "Enter":
+      case "Tab":
+        console.group("Value Added");
+        console.log(value);
+        console.groupEnd();
+        this.setState({
+          inputValue: "",
+          value: [...value, createOption(inputValue)]
+        });
+        event.preventDefault();
+    }
+  };
+
+  render() {
+    const { inputValue, value } = this.state;
+    console.log("value:", value);
+    return (
+      <CreatableSelect
+        components={components}
+        inputValue={inputValue}
+        isClearable
+        isMulti
+        menuIsOpen={false}
+        onChange={this.handleChange}
+        onInputChange={this.handleInputChange}
+        onKeyDown={this.handleKeyDown}
+        placeholder="Type something and press enter..."
+        value={this.state.value}
+        styles={colourStyles}
+      />
+    );
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,16 @@
+/**
+  This CodeSandbox has been automatically generated using
+  `react-codesandboxer`. If you're curious how that happened, you can
+  check out our docs here: https://github.com/noviny/react-codesandboxer
+
+  If you experience any struggles with this sandbox, please raise an issue
+  on github. :)
+*/
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Example from './example';
+
+ReactDOM.render(
+<Example />,
+document.getElementById('root')
+);
